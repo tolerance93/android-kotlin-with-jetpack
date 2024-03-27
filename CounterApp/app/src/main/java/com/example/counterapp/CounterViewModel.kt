9cc00dcc93779bd304df3131182fd2fa75f8204e
Tolerance <zpgy77@gmail.com>
2024-03-27 23:03:38 +0900

    MVVM archi

--- CounterApp/app/src/main/java/com/example/counterapp/CounterViewModel.kt
+++ CounterApp/app/src/main/java/com/example/counterapp/CounterViewModel.kt
@@ -4,17 +4,20 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
-class CounterViewModel : ViewModel() {
+class CounterViewModel() : ViewModel() {
 
-    private val _count = mutableStateOf(0)
+    private val _repository: CounterRepository = CounterRepository()
+    private val _count = mutableStateOf(_repository.getCounter().count)
     val count: MutableState<Int> = _count
 
     fun increment() {
-        _count.value++
+        _repository.incrementCounter()
+        _count.value = _repository.getCounter().count
     }
 
     fun decrement() {
-        _count.value--
+        _repository.decrementCounter()
+        _count.value = _repository.getCounter().count
     }
 
 
